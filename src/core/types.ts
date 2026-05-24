9020ad4272437256def0cee451ed1d620f6e960a
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-05-24 02:25:22 -0300

    made all interfaces variables readonly

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,116 +1,116 @@
 export interface Context {
-    commandName: string;
-    user: GuildUser;
-    channel: Channel;
-    locale: string;
+    readonly commandName: string;
+    readonly user: GuildUser;
+    readonly channel: Channel;
+    readonly locale: string;
     sendMessage: (content: string) => Promise<void>;
 }
 
 export type CommandHandler = (ctx: Context) => Promise<void>;
 
 export interface GatewayResponse {
-    t: string | null;
-    s: number | null;
-    op: number;
-    d: unknown;
+    readonly t: string | null;
+    readonly s: number | null;
+    readonly op: number;
+    readonly d: unknown;
 }
 
 export interface Channel {
-    type: number;
-    topic: string | null;
-    rate_limit_per_user: number;
-    position: number;
-    permissions: string;
-    parent_id: string | null;
-    nsfw: boolean;
-    name: string;
-    last_message_id: string | null;
-    id: string;
-    guild_id: string;
-    flags: number
+    readonly type: number;
+    readonly topic: string | null;
+    readonly rate_limit_per_user: number;
+    readonly position: number;
+    readonly permissions: string;
+    readonly parent_id: string | null;
+    readonly nsfw: boolean;
+    readonly name: string;
+    readonly last_message_id: string | null;
+    readonly id: string;
+    readonly guild_id: string;
+    readonly flags: number
 }
 
 // this is the "d" property of the payload sent on op 10 (HELLO)
 export interface HelloPayload {
-    heartbeat_interval: number;
-    _trace: string[];
+    readonly heartbeat_interval: number;
+    readonly _trace: string[];
 }
 
 // this is the "d" property of the payload sent on op 0 (DISPATCH) with t: "READY"
 export interface ReadyPayload {
-    v: number;
-    user_settings: {};
-    user: DiscordUser;
-    application: {
-        id: string;
-        flags: number;
+    readonly v: number;
+    readonly user_settings: {};
+    readonly user: DiscordUser;
+    readonly application: {
+        readonly id: string;
+        readonly flags: number;
     }
 }
 
 export interface GuildUser {
-    username: string;
-    public_flags: number;
-    primary_guild: Clan | null;
-    id: string;
-    global_name: string;
-    display_name_styles: any[] | null;
-    discriminator: string;
-    collectibles : any[] | null;
-    clan: Clan | null;
-    avatar_decoration_data : any | null;
-    avatar: string | null;
+    readonly username: string;
+    readonly public_flags: number;
+    readonly primary_guild: Clan | null;
+    readonly id: string;
+    readonly global_name: string;
+    readonly display_name_styles: any[] | null;
+    readonly discriminator: string;
+    readonly collectibles : any[] | null;
+    readonly clan: Clan | null;
+    readonly avatar_decoration_data : any | null;
+    readonly avatar: string | null;
 }
 
 export interface Member {
-    user: GuildUser;
-    roles: string[];
-    nick: string | null;
-    permissions: string;
-    joined_at: string;
+    readonly user: GuildUser;
+    readonly roles: string[];
+    readonly nick: string | null;
+    readonly permissions: string;
+    readonly joined_at: string;
 }
 
 export interface Clan {
-    tag: string;
-    identity_guild_id: string;
-    identity_enabled: boolean;
-    badge: string;
+    readonly tag: string;
+    readonly identity_guild_id: string;
+    readonly identity_enabled: boolean;
+    readonly badge: string;
 }
 
 export interface DiscordUser {
-    verified: boolean;
-    username: string;
-    primary_guild: string | null;
-    mfa_enabled: boolean;
-    id: string;
-    global_name: string | null;
-    flags: number;
-    email: string | null;
-    discriminator: string;
-    clan: string | null;
-    bot: boolean;
-    avatar: string | null;
+    readonly verified: boolean;
+    readonly username: string;
+    readonly primary_guild: string | null;
+    readonly mfa_enabled: boolean;
+    readonly id: string;
+    readonly global_name: string | null;
+    readonly flags: number;
+    readonly email: string | null;
+    readonly discriminator: string;
+    readonly clan: string | null;
+    readonly bot: boolean;
+    readonly avatar: string | null;
 }
 
 // 'd' property of payload on .t == "INTERACTION_CREATE"
 export interface InteractionPayload {
-    id: string;
-    token: string;
-    type: number;
-    guild_id: string;
-    channel_id: string;
-    locale: string;
-    member: Member;
-    data: {
-        name: string;
-        type: number;
-        id: string;
+    readonly id: string;
+    readonly token: string;
+    readonly type: number;
+    readonly guild_id: string;
+    readonly channel_id: string;
+    readonly locale: string;
+    readonly member: Member;
+    readonly data: {
+        readonly name: string;
+        readonly type: number;
+        readonly id: string;
     };
-    channel: Channel;
+    readonly channel: Channel;
 }
 
 export interface Command {
-    name: string;
-    description: string;
-    handler: CommandHandler;
-    guildId?: string;
+    readonly name: string;
+    readonly description: string;
+    readonly handler: CommandHandler;
+    readonly guildId?: string;
 }
